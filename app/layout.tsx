055eb2947e7c111989a255c415b26a5866773e3d
jesseombd-ux <jesse_ombd@hotmail.com>
2025-11-20 12:08:45 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,17 @@
+import "./globals.css";
 
+export const metadata = {
+  title: "Portfólio – Jessé Oliveira",
+  description: "Portfólio profissional",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-BR">
+      <body>
+        <h1 style={{ padding: "2rem" }}>LAYOUT OK</h1>
+        {children}
+      </body>
+    </html>
+  );
+}
