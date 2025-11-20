b8c69895fc9b3e60c68fbffd8e5af578088be5af
jesseombd-ux <jesse_ombd@hotmail.com>
2025-11-20 11:41:18 -0300

    Delete app/[slug]/page.tsx

--- app/[slug]/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function CasePage({ params }) {
-  return (
-    <main style={{ padding: "2rem" }}>
-      <h1>Case: {params.slug}</h1>
-      <p>Conteúdo do case.</p>
-    </main>
-  );
-}
